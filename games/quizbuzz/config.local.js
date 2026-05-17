c66f72e462ebb84f41f6094d3805fe18dac1a0cd
viswadeep-sarangi <viswadeep-sarangi@users.noreply.github.com>
2026-05-17 01:55:28 +0000

    deploy: 99b2f69af768b29d6fc6e952c8dcdd7e98ddb20f

---
+++ games/quizbuzz/config.local.js
@@ -0,0 +1,9 @@
+const firebaseConfigLocal = {
+  apiKey: "AIzaSyBjUIXX9_FNFWMjn6ATWiBZjIwALjNHW2I",
+  authDomain: "pixel-arcade-lab.firebaseapp.com",
+  databaseURL: "https://pixel-arcade-lab-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "pixel-arcade-lab",
+  storageBucket: "pixel-arcade-lab.firebasestorage.app",
+  messagingSenderId: "1060123430427",
+  appId: "1:1060123430427:web:83580a625489ec4b5db2be"
+};
